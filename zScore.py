e6d6de2afd819c9159616412f0b3d828e119d086
ve6ji-code <56846893+ve6ji-code@users.noreply.github.com>
2020-11-28 16:19:21 -0700

    Create zScore.py
    Initial commit

---
+++ zScore.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Calculate thge Z Scores of a set of data 
+# z = (X – μ) / σ
+# X : Single Data Value
+# μ : Poputlation mean    -> μ = ( Σ Xi ) / N
+# σ : Standard Deviatiom  -> σ = √ (Σ(Xi -μ )/N)
+# N: Sample Count
+
+def 

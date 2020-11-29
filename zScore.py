41f6ff3a6e785a8ab16744235714af2d3c270e0f
ve6ji-code <mlewis@ve6ji.ca>
2020-11-29 13:08:59 -0700

    Created 3 functions

--- zScore.py
+++ zScore.py
@@ -1,12 +1,44 @@
 #!/usr/bin/python3
-# Calculate thge Z Scores of a set of data 
+# Calculate thge Z Scores of a set of data
 # z = (X – μ) / σ
 # X : Single Data Value
-# μ : Poputlation mean    -> μ = ( Σ Xi ) / N
-# σ : Standard Deviatiom  -> σ = √ (Σ(Xi -μ )/N)
+# μ : Population mean    -> μ = ( Σ Xi ) / N
+# σ : Standard Deviatiom  -> σ = √ (Σ(Xi -μ )^2/N)
 # N: Sample Count
-x = {1,2,3,4,5} # Your list of samples
-def zScore(x):
-  """ Calculate the Z-Score of an array of data"""
-  for
-  return zScores
+import math
+xData = [1, 2, 3, 4, 5]  # Your list of samples
+
+
+def zScore(xData):
+    """ Calculate the Z-Score of an array of data"""
+    dev = stdDev(xData)
+    print(dev)
+    avg = popMean(xData)
+    # print(avg)
+    zscr = []
+    for xi in xData:
+        X = ((xi - avg) / dev)
+        zscr.append(X)
+    return zscr
+
+
+def popMean(xData):
+    sum = 0
+    count = len(xData)
+    for xi in xData:
+        sum = sum + xi
+    return sum/count
+
+
+def stdDev(xData):
+    dev = 0
+    count = len(xData)
+    sum = 0
+    mean = popMean(xData)
+    for xi in xData:
+        sum = sum + ((xi - mean)**2)
+    dev = math.sqrt(sum/count)
+    return dev
+
+
+print(zScore(xData))

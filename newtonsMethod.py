d7c3097a3489783ba92a5cdf7443085f1621a573
ve6ji-code <mlewis@ve6ji.ca>
2020-11-30 09:38:35 -0700

    Added epsilon

--- newtonsMethod.py
+++ newtonsMethod.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 # Calculate the square root of a number using Newton's Method of Approximating the Square
-def SQRT(a):
-    # guess = a/3
-    a = float(a)
-
-    Xn = 1.0  # Initial Guess
-    iter = 60  # Number of iterations
+def SQRT(myValue):
+    """Calculate the Square root of a given number: myValue"""
+    a = float(myValue)          # cast as float
+    epsilon = 0.0000000001      # Set your desired precision
+    x_old_estimation = a-(a/4)  # Initial Guess
+    iter = 60                   # Number of iterations
     i = 0
-    while i < iter:
-        Xn = 0.5 * (Xn + a / Xn)
-        print(Xn)
+    print('Calculating the square root of : {}'.format(myValue))
+    while i < iter and x_old_estimation > 0.0:
+        # Formula for estimating x of x^2
+        x_new_estimation = 0.5 * (x_old_estimation + a / x_old_estimation)
+        diff = abs(x_old_estimation - x_new_estimation)
+        if diff < epsilon:  # Check to see if epsilon has been met
+            break  # Break oput of while loop
+        x_old_estimation = x_new_estimation  # set old estimate value
+        print('Iteration {} : {}'.format(i, x_new_estimation))
         i = i + 1
-    print("done")
-    print(a)
 
-
-SQRT(200.0000)
+SQRT(12349995.6)  # myValue
